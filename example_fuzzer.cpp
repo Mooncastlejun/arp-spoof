56f3528f8afaf1bd77280d455e16870d12578aa7
Mooncastlejun <theski2001@gmail.com>
2024-09-21 21:13:21 +0900

    change ex_fuzzer

--- example_fuzzer.cpp
+++ example_fuzzer.cpp
@@ -1,42 +1,27 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <pcap.h>
-#include "ethhdr.h"
-#include "arphdr.h"
-
-// IP와 MAC 구조체 정의 (기존의 코드에 맞춰 수정 필요)
-struct Ip {
-    uint32_t addr;
-    Ip() : addr(0) {}
-    Ip(const char* ip) { addr = inet_addr(ip); }
-    bool operator==(const Ip& other) const { return addr == other.addr; }
-    bool operator!=(const Ip& other) const { return !(*this == other); }
-};
-
-struct Mac {
-    uint8_t addr[6];
-    Mac() { memset(addr, 0, sizeof(addr)); }
-    Mac(const char* mac) {
-        sscanf(mac, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &addr[0], &addr[1], &addr[2], &addr[3], &addr[4], &addr[5]);
-    }
-    bool operator==(const Mac& other) const { return memcmp(addr, other.addr, sizeof(addr)) == 0; }
-};
+#include "mac.h"
+#include "ip.h"
 
 // Fuzzer 엔트리 포인트
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     if (size < 32) return 0; // 입력 데이터가 너무 작으면 무시
 
     // 임의의 IP 주소 생성
-    char send_ip_str[16];
-    char target_ip_str[16];
-    
-    snprintf(send_ip_str, sizeof(send_ip_str), "%d.%d.%d.%d", data[0], data[1], data[2], data[3]);
-    snprintf(target_ip_str, sizeof(target_ip_str), "%d.%d.%d.%d", data[4], data[5], data[6], data[7]);
+    Ip send_ip(std::string(std::to_string(data[0]) + "." + std::to_string(data[1]) + "." + std::to_string(data[2]) + "." + std::to_string(data[3])));
+    Ip target_ip(std::string(std::to_string(data[4]) + "." + std::to_string(data[5]) + "." + std::to_string(data[6]) + "." + std::to_string(data[7])));
+
+    // 임의의 MAC 주소 생성
+    Mac send_mac = Mac::randomMac();
+    Mac target_mac = Mac::randomMac();
 
     // ARP 스푸핑 프로그램 실행
-    char *argv[] = { "arp-spoof", "wlan0", send_ip_str, target_ip_str };
+    std::string send_ip_str = static_cast<std::string>(send_ip);
+    std::string target_ip_str = static_cast<std::string>(target_ip);
+
+    char* argv[] = { "arp-spoof", "wlan0", const_cast<char*>(send_ip_str.c_str()), const_cast<char*>(target_ip_str.c_str()) };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     // 기존 main 함수 호출 (argv와 argc 전달)
